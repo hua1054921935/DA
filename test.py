73c7b624761a0127dfb8b4f0b4bb82bbdf77fe67
sj <1174484260@qq.com>
2018-05-11 18:11:40 +0800

    提交pip

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # -*-coding:utf-8-*-
 import os
+import pycharts
 import pandas
 import glob
 # os.chdir('data/2017-11-01')
@@ -16,24 +17,23 @@ import glob
 # import os
 
 # 获取所有csv文件
-def scan_files(directory, prefix=None, postfix=None):
-    files_list = []
-
-    for root, sub_dirs, files in os.walk(directory):
-        for special_file in files:
-            if postfix:
-                if special_file.endswith(postfix):
-                    files_list.append(os.path.join(root, special_file))
-            elif prefix:
-                if special_file.startswith(prefix):
-                    files_list.append(os.path.join(root, special_file))
-            else:
-                files_list.append(os.path.join(root, special_file))
-
-    return files_list
-
-list1=scan_files('data')
-for data in list1:
-    print data
-    pandas.DataFrame()
-   
+# def scan_files(directory, prefix=None, postfix=None):
+#     files_list = []
+#
+#     for root, sub_dirs, files in os.walk(directory):
+#         for special_file in files:
+#             if postfix:
+#                 if special_file.endswith(postfix):
+#                     files_list.append(os.path.join(root, special_file))
+#             elif prefix:
+#                 if special_file.startswith(prefix):
+#                     files_list.append(os.path.join(root, special_file))
+#             else:
+#                 files_list.append(os.path.join(root, special_file))
+#
+#     return files_list
+#
+# list1=scan_files('data')
+# for data in list1:
+#     print data
+#     pandas.DataFrame()
